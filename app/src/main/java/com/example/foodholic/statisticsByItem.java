de093f3137dfd803d193c0a4bcee3671c2f4117e
mohammed alzoubi <mazzoubi@outlook.com>
2019-11-17 22:16:54 +0200

    statisticsByItem

--- app/src/main/java/com/example/foodholic/statisticsByItem.java
+++ app/src/main/java/com/example/foodholic/statisticsByItem.java
@@ -2,12 +2,139 @@ package com.example.foodholic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class statisticsByItem extends AppCompatActivity {
 
+    ArrayList<classSales> list;
+    ArrayList<classItem> listItem;
+    ArrayList<classSubItem>listSubItem;
+
+    TextView result ;
+    Spinner month,year,item,subitem;
+    String m="",y="",it="",s="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics_by_item);
+        init();
+
+        month.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i==0){
+
+                }
+                else {
+                    m=adapterView.getItemAtPosition(i).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i==0){
+
+                }
+                else {
+                    y=adapterView.getItemAtPosition(i).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        item.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i==0){
+
+                }
+                else {
+                    it=adapterView.getItemAtPosition(i).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        subitem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i==0){
+
+                }
+                else {
+                    s=adapterView.getItemAtPosition(i).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    void init(){
+        list=Adminpage.cSale;
+        listItem();
+        listSubItem();
+        result=findViewById(R.id.a1textresult);
+        month=findViewById(R.id.a1monthspinner);
+        year=findViewById(R.id.a1yearspinner);
+        item=findViewById(R.id.a1itemspinner);
+        subitem=findViewById(R.id.a1sunitemspinner);
+    }
+    void listItem(){
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+
+        db.collection("Res_1_items").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+            listItem=new ArrayList<>();
+            List<DocumentSnapshot> list=queryDocumentSnapshots.getDocuments();
+                for(DocumentSnapshot d : list){
+                    listItem.add(d.toObject(classItem.class));
+                }
+            }
+        });
+    }
+    void listSubItem(){
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("Res_1_subItems").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                listSubItem=new ArrayList<>();
+                List<DocumentSnapshot> list=queryDocumentSnapshots.getDocuments();
+                for(DocumentSnapshot d : list){
+                    listSubItem.add(d.toObject(classSubItem.class));
+                }
+            }
+        });
     }
 }
