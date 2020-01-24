a46262687e4858f9c702cf87cc7f5517c668b050
mohammed alzoubi <mazzoubi@outlook.com>
2020-01-24 22:50:20 +0200

    Merge branch 'master' of D:\androidProjectTest\git with conflicts.

---
+++ app/src/main/java/com/example/foodholic/classVipUser.java
@@ -0,0 +1,5 @@
+package com.example.foodholic;
+
+public class classVipUser {
+    public String name ="", phone="", email="", loc="",listItem="";
+}
---
+++ app/src/main/java/com/example/foodholic/vipAddUser.java
@@ -0,0 +1,13 @@
+package com.example.foodholic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vipAddUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vip_add_user);
+    }
+}
